c694e0ec233ff66dadf9bcac89889c00540f8cf2
Martin Janů <martin.janu@protonmail.com>
2021-03-28 12:32:09 +0200

    Sort types into logical groups.

--- parse.py
+++ parse.py
@@ -83,22 +83,22 @@ def main():
         "walksize"]
 
     KNOWN_TYPES = [
+        "cipher",
         "ablkcipher",
-        "aead",
-        "ahash",
         "akcipher",
         "blkcipher",
-        "cipher",
+        "givcipher",
+        "skcipher",
+        "aead",
+        "nivaead",
+        "ahash",
+        "shash",
         "compression",
         "digest",
-        "givcipher",
         "kpp",
-        "nivaead",
         "pcomp",
-        "rng",
         "scomp",
-        "shash",
-        "skcipher"]
+        "rng"]
 
 
     ciphers = []
