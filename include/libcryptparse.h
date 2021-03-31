a9a3c7044945ed62323851fad4d86d671646e456
Martin Janů <martin.janu@protonmail.com>
2021-03-31 13:38:42 +0200

    Change name prefix, parse function interface

--- include/libcryptparse.h
+++ include/libcryptparse.h
@@ -6,53 +6,53 @@
 #include <stdint.h>
 #include <stdio.h>
 
-enum crypto_type {
-	crypto_type_unknown,
-	crypto_type_cipher,
-        crypto_type_ablkcipher,
-        crypto_type_akcipher,
-        crypto_type_blkcipher,
-        crypto_type_givcipher,
-        crypto_type_skcipher,
-        crypto_type_aead,
-        crypto_type_nivaead,
-        crypto_type_ahash,
-        crypto_type_shash,
-        crypto_type_compression,
-        crypto_type_digest,
-        crypto_type_kpp,
-        crypto_type_pcomp,
-        crypto_type_scomp,
-        crypto_type_rng
+enum cryptparse_type {
+	cryptparse_type_unknown,
+	cryptparse_type_cipher,
+        cryptparse_type_ablkcipher,
+        cryptparse_type_akcipher,
+        cryptparse_type_blkcipher,
+        cryptparse_type_givcipher,
+        cryptparse_type_skcipher,
+        cryptparse_type_aead,
+        cryptparse_type_nivaead,
+        cryptparse_type_ahash,
+        cryptparse_type_shash,
+        cryptparse_type_compression,
+        cryptparse_type_digest,
+        cryptparse_type_kpp,
+        cryptparse_type_pcomp,
+        cryptparse_type_scomp,
+        cryptparse_type_rng
 };
 
-enum crypto_alg_fields {
-	crypto_alg_type =        0x00001,
-	crypto_alg_name =        0x00002,
-	crypto_alg_driver =      0x00004,
-	crypto_alg_module =      0x00008,
-	crypto_alg_priority =    0x00010,
-	crypto_alg_refcnt =      0x00020,
-	crypto_alg_async =       0x00040,
-	crypto_alg_blocksize =   0x00080,
-	crypto_alg_chunksize =   0x00100,
-	crypto_alg_digestsize =  0x00200,
-	crypto_alg_geniv =       0x00400,
-	crypto_alg_internal =    0x00800,
-	crypto_alg_ivsize =      0x01000,
-	crypto_alg_maxauthsize = 0x02000,
-	crypto_alg_max_keysize = 0x04000,
-	crypto_alg_min_keysize = 0x08000,
-	crypto_alg_seedsize =    0x10000,
-	crypto_alg_selftest =    0x20000,
-	crypto_alg_walksize =    0x40000
+enum cryptparse_alg_fields {
+	cryptparse_alg_type =        0x00001,
+	cryptparse_alg_name =        0x00002,
+	cryptparse_alg_driver =      0x00004,
+	cryptparse_alg_module =      0x00008,
+	cryptparse_alg_priority =    0x00010,
+	cryptparse_alg_refcnt =      0x00020,
+	cryptparse_alg_async =       0x00040,
+	cryptparse_alg_blocksize =   0x00080,
+	cryptparse_alg_chunksize =   0x00100,
+	cryptparse_alg_digestsize =  0x00200,
+	cryptparse_alg_geniv =       0x00400,
+	cryptparse_alg_internal =    0x00800,
+	cryptparse_alg_ivsize =      0x01000,
+	cryptparse_alg_maxauthsize = 0x02000,
+	cryptparse_alg_max_keysize = 0x04000,
+	cryptparse_alg_min_keysize = 0x08000,
+	cryptparse_alg_seedsize =    0x10000,
+	cryptparse_alg_selftest =    0x20000,
+	cryptparse_alg_walksize =    0x40000
 };
 
-struct crypto_alg {
+struct cryptparse_alg {
 	// Used fields mask
 	uint32_t used_fields;
 	// Required fields
-	char *type; // change to crypto_type
+	char *type; // change to cryptparse_type
 	char *name;
 	char *driver;
 	char *module;
@@ -74,6 +74,6 @@ struct crypto_alg {
 	unsigned walksize;
 };
 
-int crypto_parse(FILE *fp, struct crypto_alg **algorithms);
+int cryptparse_parse(char *path, struct cryptparse_alg **algorithms);
 
 #endif // LIBCRYPTPARSE_H
