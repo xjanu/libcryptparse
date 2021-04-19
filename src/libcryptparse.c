feec097c03f7ff11e3cf72dfdc6cf7bcac499873
Martin Janů <martin.janu@protonmail.com>
2021-04-19 15:03:08 +0200

    Use a lookup table for parsing fields

--- src/libcryptparse.c
+++ src/libcryptparse.c
@@ -3,6 +3,8 @@
 #define _POSIX_C_SOURCE 200809L
 
 #include <assert.h>
+#include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,11 +13,65 @@
 
 // TODO: Check all mallocs
 
-static char *_alg_string_thing(char *value) {
-	char *ret = calloc(strlen(value) + 1, sizeof(char));
-	if (ret != NULL)
-		strcpy(ret, value);
-	return ret;
+// Used for binding strings to fields
+struct field_binding {
+	char *string;
+	enum cryptparse_alg_fields field;
+};
+
+// Lookup table for matching a string with a field it represents
+static const struct field_binding field_lookup[] = {
+	{"type",        cryptparse_alg_type},
+	{"name",        cryptparse_alg_name},
+	{"driver",      cryptparse_alg_driver},
+	{"module",      cryptparse_alg_module},
+	{"priority",    cryptparse_alg_priority},
+	{"refcnt",      cryptparse_alg_refcnt},
+	{"async",       cryptparse_alg_async},
+	{"blocksize",   cryptparse_alg_blocksize},
+	{"chunksize",   cryptparse_alg_chunksize},
+	{"digestsize",  cryptparse_alg_digestsize},
+	{"geniv",       cryptparse_alg_geniv},
+	{"internal",    cryptparse_alg_internal},
+	{"ivsize",      cryptparse_alg_ivsize},
+	{"maxauthsize", cryptparse_alg_maxauthsize},
+	{"max_keysize", cryptparse_alg_max_keysize},
+	{"min_keysize", cryptparse_alg_min_keysize},
+	{"seedsize",    cryptparse_alg_seedsize},
+	{"selftest",    cryptparse_alg_selftest},
+	{"walksize",    cryptparse_alg_walksize}
+};
+
+static bool value_parser_string(char **res, char *value) {
+	*res = calloc(strlen(value) + 1, sizeof(char));
+	if (*res == NULL)
+		return false;
+	strcpy(*res, value);
+	return true;
+}
+
+static bool value_parser_bool(bool *res, char *value,
+                              const char *trueval, const char *falseval)
+{
+	if (strcmp(value, trueval) == 0) {
+		*res = true;
+	} else if (strcmp(value, falseval) == 0) {
+		*res = false;
+	} else {
+		errno = EINVAL;
+		return false;
+	}
+	return true;
+}
+
+static bool value_parser_unsigned(unsigned *res, char *value)
+{
+	errno = 0;
+	unsigned long big = strtoul(value, NULL, 10);
+	if (big > UINT_MAX)
+		errno = ERANGE;
+	*res = big;
+	return errno == 0 ? true : false;
 }
 
 static void _alg_destroy(struct cryptparse_alg *algorithm) {
@@ -32,133 +88,125 @@ static void _alg_destroy(struct cryptparse_alg *algorithm) {
 
 static int cryptparse_alg_parse(FILE *fp, struct cryptparse_alg *algorithm)
 {
+	// TODO: Better decomposition
+	// TODO: Better error checking and reporting
+	int ret = 0;
+
 	algorithm->used_fields = 0;
 
-	char *field, *value, *line = NULL;
+	char *line = NULL;
 	size_t line_buf_len = 0;
 	ssize_t nread;
 
 	while ((nread = getline(&line, &line_buf_len, fp)) != -1) {
 		if (nread == 1)
 			break;
+		assert(line[nread - 1] == '\n');
 		line[nread-1] = '\0';
 
-		char* colon = strchr(line, ':');
+		char *colon = strchr(line, ':');
 		assert(colon != NULL);
 
-		field = line;
+		char *field = line;
 		for (char* i = colon - 1; i >= field && *i == ' '; --i)
 			*i = '\0';
 
 		assert(*colon == ':');
 		assert(*(colon + 1) == ' ');
-		value = colon + 2;
+		assert(line[nread - 1] == '\0');
+		line[nread] = '\0';
+		char *value = colon + 2;
 
-		// TODO: Fix vvv (Perhaps use a lookup table?)
-		//       Also use helper functions for unsigned/bool conversions
-		// This is horrid and i know it :(
-		if (strcmp(field, "type") == 0) {
-			// TODO: Fix the string copying
-			algorithm->type = _alg_string_thing(value);
-			algorithm->used_fields |= cryptparse_alg_type;
+		int alg_field = 0;
+		for (size_t i = 0;
+		     i < sizeof(field_lookup)/ sizeof(struct field_binding);
+		     ++i) {
+			if (strcmp(field, field_lookup[i].string) == 0) {
+				alg_field = field_lookup[i].field;
+				break;
+			}
+		}
+		assert(alg_field != 0);
+		assert((algorithm->used_fields & alg_field) == 0);
 
-		} else if (strcmp(field, "name") == 0) {
-			algorithm->name = _alg_string_thing(value);
-			algorithm->used_fields |= cryptparse_alg_name;
+		algorithm->used_fields |= alg_field;
 
-		} else if (strcmp(field, "driver") == 0) {
-			algorithm->driver = _alg_string_thing(value);
-			algorithm->used_fields |= cryptparse_alg_driver;
-
-		} else if (strcmp(field, "module") == 0) {
-			algorithm->module = _alg_string_thing(value);
-			algorithm->used_fields |= cryptparse_alg_module;
-
-		} else if (strcmp(field, "priority") == 0) {
-			algorithm->priority = strtoul(value, NULL, 10);
-			algorithm->used_fields |= cryptparse_alg_priority;
-
-		} else if (strcmp(field, "refcnt") == 0) {
-			algorithm->refcnt = strtoul(value, NULL, 10);
-			algorithm->used_fields |= cryptparse_alg_refcnt;
-
-		} else if (strcmp(field, "async") == 0) {
-			if (strcmp(value, "yes") == 0)
-				algorithm->async = true;
-			else if (strcmp(value, "no") == 0)
-				algorithm->async = false;
-			else
-				assert(false);
-			algorithm->used_fields |= cryptparse_alg_async;
-
-		} else if (strcmp(field, "blocksize") == 0) {
-			algorithm->blocksize = strtoul(value, NULL, 10);
-			algorithm->used_fields |= cryptparse_alg_blocksize;
-
-		} else if (strcmp(field, "chunksize") == 0) {
-			algorithm->chunksize = strtoul(value, NULL, 10);
-			algorithm->used_fields |= cryptparse_alg_chunksize;
-
-		} else if (strcmp(field, "digestsize") == 0) {
-			algorithm->digestsize = strtoul(value, NULL, 10);
-			algorithm->used_fields |= cryptparse_alg_digestsize;
-
-		} else if (strcmp(field, "geniv") == 0) {
-			algorithm->geniv = _alg_string_thing(value);
-			algorithm->used_fields |= cryptparse_alg_geniv;
-
-		} else if (strcmp(field, "internal") == 0) {
-			if (strcmp(value, "yes") == 0)
-				algorithm->internal = true;
-			else if (strcmp(value, "no") == 0)
-				algorithm->internal = false;
-			else
-				assert(false);
-			algorithm->used_fields |= cryptparse_alg_internal;
-
-		} else if (strcmp(field, "ivsize") == 0) {
-			algorithm->ivsize = strtoul(value, NULL, 10);
-			algorithm->used_fields |= cryptparse_alg_ivsize;
-
-		} else if (strcmp(field, "maxauthsize") == 0) {
-			algorithm->maxauthsize = strtoul(value, NULL, 10);
-			algorithm->used_fields |= cryptparse_alg_maxauthsize;
-
-		} else if (strcmp(field, "max_keysize") == 0) {
-			algorithm->max_keysize = strtoul(value, NULL, 10);
-			algorithm->used_fields |= cryptparse_alg_max_keysize;
-
-		} else if (strcmp(field, "min_keysize") == 0) {
-			algorithm->min_keysize = strtoul(value, NULL, 10);
-			algorithm->used_fields |= cryptparse_alg_min_keysize;
-
-		} else if (strcmp(field, "seedsize") == 0) {
-			algorithm->seedsize = strtoul(value, NULL, 10);
-			algorithm->used_fields |= cryptparse_alg_seedsize;
-
-		} else if (strcmp(field, "selftest") == 0) {
-			if (strcmp(value, "passed") == 0)
-				algorithm->selftest = true;
-			// TODO: Find out the real printed value if test fails
-			else if (strcmp(value, "failed") == 0)
-				algorithm->selftest = false;
-			else
-				assert(false);
-			algorithm->used_fields |= cryptparse_alg_selftest;
-
-		} else if (strcmp(field, "walksize") == 0) {
-			algorithm->walksize = strtoul(value, NULL, 10);
-			algorithm->used_fields |= cryptparse_alg_walksize;
-
-		} else {
-			// Unknown field
+		bool parser_ret;
+		switch(alg_field) {
+		// String members
+		case cryptparse_alg_type:
+			parser_ret = value_parser_string(&algorithm->type, value);
+			break;
+		case cryptparse_alg_name:
+			parser_ret = value_parser_string(&algorithm->name, value);
+			break;
+		case cryptparse_alg_driver:
+			parser_ret = value_parser_string(&algorithm->driver, value);
+			break;
+		case cryptparse_alg_module:
+			parser_ret = value_parser_string(&algorithm->module, value);
+			break;
+		case cryptparse_alg_geniv:
+			parser_ret = value_parser_string(&algorithm->geniv, value);
+			break;
+		// Bool members
+		case cryptparse_alg_async:
+			parser_ret = value_parser_bool(&algorithm->async, value, "yes", "no");
+			break;
+		case cryptparse_alg_internal:
+			parser_ret = value_parser_bool(&algorithm->internal, value, "yes", "no");
+			break;
+		case cryptparse_alg_selftest:
+			parser_ret = value_parser_bool(&algorithm->selftest, value, "passed", "failed");
+			break;
+		// Unsigned members
+		case cryptparse_alg_priority:
+			parser_ret = value_parser_unsigned(&algorithm->priority, value);
+			break;
+		case cryptparse_alg_refcnt:
+			parser_ret = value_parser_unsigned(&algorithm->refcnt, value);
+			break;
+		case cryptparse_alg_blocksize:
+			parser_ret = value_parser_unsigned(&algorithm->blocksize, value);
+			break;
+		case cryptparse_alg_chunksize:
+			parser_ret = value_parser_unsigned(&algorithm->chunksize, value);
+			break;
+		case cryptparse_alg_digestsize:
+			parser_ret = value_parser_unsigned(&algorithm->digestsize, value);
+			break;
+		case cryptparse_alg_ivsize:
+			parser_ret = value_parser_unsigned(&algorithm->ivsize, value);
+			break;
+		case cryptparse_alg_maxauthsize:
+			parser_ret = value_parser_unsigned(&algorithm->maxauthsize, value);
+			break;
+		case cryptparse_alg_max_keysize:
+			parser_ret = value_parser_unsigned(&algorithm->max_keysize, value);
+			break;
+		case cryptparse_alg_min_keysize:
+			parser_ret = value_parser_unsigned(&algorithm->min_keysize, value);
+			break;
+		case cryptparse_alg_seedsize:
+			parser_ret = value_parser_unsigned(&algorithm->seedsize, value);
+			break;
+		case cryptparse_alg_walksize:
+			parser_ret = value_parser_unsigned(&algorithm->walksize, value);
+			break;
+		default:
 			assert(false);
+		}
+		if (!parser_ret) {
+			fprintf(stderr, "%s ", field);
+			perror("value_parser");
+			ret = 1;
+			goto out;
 		}
 	}
 
+out:
 	free(line);
-
-	return 0;
+	return ret;
 }
 
 int cryptparse_parse(char *path, struct cryptparse_alg **algorithms)
